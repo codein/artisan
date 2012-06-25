bb6ff0d6aa2b8986abc92559cb0efb4356eef7b1
codein <robin.codein@gmail.com>
2012-06-24 21:17:22 -0400

    add image_merge function

--- core/py_polygon.py
+++ core/py_polygon.py
@@ -7,7 +7,7 @@ from operator import itemgetter
 import Image
 import Polygon, Polygon.IO
 import ImageOps
-
+import os
 
 
 
@@ -63,7 +63,14 @@ class PolygonUtil():
         outputs = []        
         surface = Polygon.Shapes.Circle(radius = (2*qradius)+tradius, center = (tx, ty))
         surface = surface - t
-        for i in range(int(tradius/2),int(qradius+tradius)):
+        start_radius = 0
+        for i in range(int(qradius+tradius)):
+            c = Polygon.Shapes.Circle(radius = i, center = t.center())
+            if not t.covers(c):
+                start_radius = i
+                break
+            
+        for i in range(start_radius,int(qradius+tradius)):
 #            print i,t.center()      
             c = Polygon.Shapes.Circle(radius = i, center = t.center())
             for (x,y) in c.contour(0):
@@ -94,8 +101,10 @@ class PolygonUtil():
         print width,height
         pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
         p = Polygon.Polygon()
-        contours_first = []
-        contours_last = []
+        x_contours_first = []
+        x_contours_last = []
+        y_contours_first = []
+        y_contours_last = []
         for y in range(width-1):
             first,last = 0,0
             for x in range(height-1):
@@ -105,14 +114,14 @@ class PolygonUtil():
                     else:
                         last = (x,y)
             if first != 0:
-                contours_first.append(first)
+                x_contours_first.append(first)
             if last != 0:
-                contours_last.append(last)
-#            print first,last,'\n'        
-        contours_last.reverse()        
-        p.addContour(contours_first+contours_last)
+                x_contours_last.append(last)
+        x_contours_last.reverse()
+        
+        p.addContour(x_contours_first+x_contours_last)
         xmin, xmax, ymin, ymax = p.boundingBox()
-        print abs(xmin - xmax), abs(ymin - ymax)
+#        print abs(xmin - xmax), abs(ymin - ymax)
         return p         
     
     def convert_to_alpha(self,im):
@@ -126,6 +135,105 @@ class PolygonUtil():
                 newData.append(item)
         img.putdata(newData)
         return img
+
+    """Fit image fileq around filet and returns best possible fit with no overlap"""
+    def image_merge(self,filet,fileq,output_count=5,write=False,show=False):
+     
+
+        print 'Merging %s & %s for %d outputs' % (filet, fileq, output_count)
+        t_im= Image.open(filet)
+        q_im= Image.open(fileq)
+        t_im=poly_util.convert_to_alpha(t_im)
+        q_im=poly_util.convert_to_alpha(q_im)
+    #    t_im = t_im.rotate(90)
+    #    q_im = q_im.rotate(90)
+        q_width, q_height = q_im.size
+        t_width, t_height = t_im.size 
+        
+        t = poly_util.getPolygon(filet)
+        Polygon.IO.writeSVG('testt.svg', (t,))
+        q = poly_util.getPolygon(fileq)
+        Polygon.IO.writeSVG('testq.svg', (q,))
+        
+        #fits polygon q arounf t
+        fit_config = poly_util.fit(q,t,output_count=output_count)
+        x,y = fit_config.get('center')
+        angle =fit_config.get('angle')
+        #offset the polygon for suggested fit
+        poly_util.redraw(t, x, y)
+        t.rotate(fit_config.get('angle'), x, y)
+    #####before shifting    
+    #    print 'q bounding'
+    #    xmin, xmax, ymin, ymax = q.boundingBox()
+    #    print xmin, xmax, ymin, ymax
+    #    print abs(xmin - xmax), abs(ymin - ymax)
+    #    
+    #    print 't bounding'
+        xmin, xmax, ymin, ymax = t.boundingBox()
+    #    print xmin, xmax, ymin, ymax
+    #    print abs(xmin - xmax), abs(ymin - ymax)    
+        #polygons are shifted so that both polygons after merge lie in the first quadrant
+        offset_x,offset_y = 0,0
+        if xmin<0:
+            offset_x = -xmin
+        if ymin <0:
+            offset_y = -ymin        
+        t_x,t_y = t.center()
+        poly_util.redraw(t, t_x+offset_x, t_y+offset_y)
+        q_x,q_y = q.center()
+        poly_util.redraw(q, q_x+offset_x, q_y+offset_y)
+    #####after shifting     
+    #    print 'q bounding'
+        qxmin, qxmax, qymin, qymax = q.boundingBox()
+    #    print qxmin, qxmax, qymin, qymax
+    #    
+    #    print 't bounding'
+        txmin, txmax, tymin, tymax = t.boundingBox()
+    #    print txmin, txmax, tymin, tymax
+         
+        output = q + t
+        
+        #merge the image to reflect suggested fit_config of output polygon
+        base_im = Image.open('/home/codein/workspace3/artist/base.png')
+        
+    #    print 'output bounding'
+        outputxmin, outputxmax, outputymin, outputymax = output.boundingBox()
+#        print  outputxmin, outputxmax, outputymin, outputymax
+        #render a base image big enought o hold both the images after merging
+        base_width, base_height =  int(max((q_width+qymin),(t_width+tymin))), int(max((q_height+qxmin),(t_height+txmin)))
+        base_im = base_im.resize((base_width, base_height))
+        base_im=poly_util.convert_to_alpha(base_im)
+        
+        offset_y = int(base_height-qymin)
+        q_box = (0,0,q_width,q_height)
+        #offset for polygon q and orientation, since the output polygon is potted at a 90 rotated plane
+        q_box_offset =  (int(0+qymin),int(0+qxmin),int(q_width+qymin),int(q_height+qxmin))    
+     
+    #    print '\n\nmove to',qxmin,  qymin, qxmax,qymax
+    #    print 'qbox cut at%s'%str(q_box)
+    #    print 'qbox paste at%s'%str(q_box_offset)
+        q_region = q_im.crop(q_box)
+        #cut the image from the original image and paste it at the offset location
+        base_im.paste(q_region,q_box_offset,q_region)
+        
+        offset_y = int(base_height-tymin)
+        t_box = (0,0,t_width,t_height)
+        t_box_offset =  (int(0+tymin),int(0+txmin),int(t_width+tymin),int(t_height+txmin))    
+    #    print '\n\nmove to',txmin,  tymin, txmax,tymax
+    #    print 'tbox cut at%s'%str(t_box)
+    #    print 'tbox paste at%s'%str(t_box_offset)
+        t_region = t_im.crop(t_box)
+        base_im.paste(t_region,t_box_offset,t_region)
+        
+        if show:
+            base_im.show()
+        
+        if write:
+            Polygon.IO.writeSVG('testb.svg', (output,))
+            base_im.save("img2.png", "PNG")
+        print 'Merged %s %s' % (filet,fileq)
+        return base_im
+    
          
         
 poly_util = PolygonUtil()
@@ -153,112 +261,35 @@ def polygon_merge():
     Polygon.IO.writeSVG('testb.svg', (q,))
     
 def image_read():
-    c = Polygon.Shapes.Circle(radius = 1, center = (0,0))
-    d = Polygon.Shapes.Circle(radius = 1, center = (5,0))
-    e = Polygon.Shapes.Circle(radius = 1, center = (10,0))
-    f = Polygon.Shapes.Circle(radius = 1, center = (0,0))
-    g = Polygon.Shapes.Circle(radius = 1, center = (0,5))
-    h = Polygon.Shapes.Circle(radius = 1, center = (0,10))    
-    o = c+d+e+f+g+h
-    Polygon.IO.writeSVG('o.svg', (o,))
-
-def image_merge():
- 
-    filenamet,filenameq= '/home/codein/workspace3/artist/Untitled.png','/home/codein/workspace3/artist/celctic.png'
-#    filenameq,filenamet='/home/codein/workspace3/artist/rectangle.png','/home/codein/workspace3/artist/triangle1.png'
-    filenameq,filenamet=    '/home/codein/workspace3/artist/Celtic_Tribal_Howling_Coyote_by_WildSpiritWolf.png','/home/codein/workspace3/artist/Eagle_Tribal_Tattoo_by_Debaser2020.jpg'
-    filenameq,filenamet= '/home/codein/workspace3/artist/Eagle-Tribal-Tattoo.bmp','/home/codein/workspace3/artist/img2.png'
+    file = '/home/codein/artist/test1/eagle-tribal-tattoo-design-free-designs--2-1-tattoodonkey.com.jpg'
+    t = poly_util.getPolygon(file)
+    Polygon.IO.writeSVG('/home/codein/artist/test1/testb.svg', (t,))
     
-    t_im= Image.open(filenamet)
-    q_im= Image.open(filenameq)
-    t_im=poly_util.convert_to_alpha(t_im)
-    q_im=poly_util.convert_to_alpha(q_im)
-#    t_im = t_im.rotate(90)
-#    q_im = q_im.rotate(90)
-    q_width, q_height = q_im.size
-    t_width, t_height = t_im.size 
+def iamge_merge():
+    filet,fileq = '/home/codein/artist/test1/eagle-tribal-tattoo-design-free-designs--2-1-tattoodonkey.com.jpg','/home/codein/artist/test1/Inca_Bird_Tattoo_by_Rustyoldtown.jpg'
+    base = poly_util.image_merge(filet, fileq, 5, True)
+    base_file ="%s/Merging.png" % '/home/codein/artist/test1/'    
     
-    t = poly_util.getPolygon(filenamet)
-    Polygon.IO.writeSVG('testt.svg', (t,))
-    q = poly_util.getPolygon(filenameq)
-    Polygon.IO.writeSVG('testq.svg', (q,))
-    fit_config = poly_util.fit(q,t,output_count=1)
-    x,y = fit_config.get('center')
-    angle =fit_config.get('angle')
-    
-    
-    
-    poly_util.redraw(t, x, y)
-    t.rotate(fit_config.get('angle'), x, y)
-    print 'q bounding'
-    xmin, xmax, ymin, ymax = q.boundingBox()
-    print xmin, xmax, ymin, ymax
-    print abs(xmin - xmax), abs(ymin - ymax)
-    
-    print 't bounding'
-    xmin, xmax, ymin, ymax = t.boundingBox()
-    print xmin, xmax, ymin, ymax
-    print abs(xmin - xmax), abs(ymin - ymax)
-    
-    offset_x,offset_y = 0,0
-    if xmin<0:
-        offset_x = -xmin
-    if ymin <0:
-        offset_y = -ymin    
-    
-    t_x,t_y = t.center()
-    poly_util.redraw(t, t_x+offset_x, t_y+offset_y)
-    q_x,q_y = q.center()
-    poly_util.redraw(q, q_x+offset_x, q_y+offset_y)
-    
-    print 'q bounding'
-    qxmin, qxmax, qymin, qymax = q.boundingBox()
-    print qxmin, qxmax, qymin, qymax
-    
-    print 't bounding'
-    txmin, txmax, tymin, tymax = t.boundingBox()
-    print txmin, txmax, tymin, tymax
-    
-    output = q + t
-
-    base_im = Image.open('/home/codein/workspace3/artist/base.png')
-    
-    print 'output bounding'
-    outputxmin, outputxmax, outputymin, outputymax = output.boundingBox()
-    print  outputxmin, outputxmax, outputymin, outputymax
-    base_width, base_height =  int(max((q_width+qymin),(t_width+tymin))), int(max((q_height+qxmin),(t_height+txmin)))
-    base_im = base_im.resize((base_width, base_height))
-
-    offset_y = int(base_height-qymin)
-    q_box = (0,0,q_width,q_height)
-    q_box_offset =  (int(0+qymin),int(0+qxmin),int(q_width+qymin),int(q_height+qxmin))    
- 
-    print '\n\nmove to',qxmin,  qymin, qxmax,qymax
-    print 'qbox cut at%s'%str(q_box)
-    print 'qbox paste at%s'%str(q_box_offset)
-    q_region = q_im.crop(q_box)
-    base_im.paste(q_region,q_box_offset,q_region)
-    
-    offset_y = int(base_height-tymin)
-    t_box = (0,0,t_width,t_height)
-    t_box_offset =  (int(0+tymin),int(0+txmin),int(t_width+tymin),int(t_height+txmin))    
-    print '\n\nmove to',txmin,  tymin, txmax,tymax
-    print 'tbox cut at%s'%str(t_box)
-    print 'tbox paste at%s'%str(t_box_offset)
-    t_region = t_im.crop(t_box)
-    base_im.paste(t_region,t_box_offset,t_region)
-    
-    base_im.show()
-    
-
-    Polygon.IO.writeSVG('testb.svg', (output,))
-    base_im.save("img2.png", "PNG")
-
-
+def images_merge():         
+    path = '/home/codein/artist/test1'
+    listing = os.listdir(path)
+    base = None
+    count = 0
+    for infile in listing:
+        file = "%s/%s" %( path,infile)
+        if not base:
+            base = file            
+        else:
+            count+=1
+            print base,file
+            base = poly_util.image_merge(base, file, 5)
+            base_file ="%s/Merging.png" % path
+            base.save(base_file, "PNG")
+            base = base_file            
 
     
 if __name__ == "__main__":
-    image_merge()
+    images_merge()
 
 
 
